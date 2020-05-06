07ea86aa6c53b9decc0917e36c774cb75bc44a44
Thad Hoskin <thad.hoskin-01@gmail.com>
2020-05-06 21:31:43 +1000

    Added a controller and starting to fill the basic controls out

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,25 @@
+class MoviesController < ApplicationController
+
+  def index
+  end
+
+  def show
+    
+  end
+
+  def create
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :movies
   devise_for :users
   root to: 'home#index'
 end
