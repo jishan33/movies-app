d435ceeda2b4697d8842cf381d83e249c7e49a3c
Ji Shan <uug54332017@gmail.com>
2020-05-06 20:45:13 +1000

    director model added

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ApplicationRecord
   validates :name, presence: true
   validates :release_year, presence: true, numericality: { greater_than: 1888 }
+  has_many :genre_movies
+  has_many :genres, through: :genre_movies
+  belongs_to :director, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_06_094736) do
+ActiveRecord::Schema.define(version: 2020_05_06_102024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "directors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "genre_movies", force: :cascade do |t|
     t.bigint "movie_id", null: false
@@ -38,6 +44,8 @@ ActiveRecord::Schema.define(version: 2020_05_06_094736) do
     t.string "length"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "director_id", null: false
+    t.index ["director_id"], name: "index_movies_on_director_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -54,4 +62,5 @@ ActiveRecord::Schema.define(version: 2020_05_06_094736) do
 
   add_foreign_key "genre_movies", "genres"
   add_foreign_key "genre_movies", "movies"
+  add_foreign_key "movies", "directors"
 end
