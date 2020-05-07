d17e2c496c71620a27aed7db50950d24f7c1f3c0
Thad Hoskin <thad.hoskin-01@gmail.com>
2020-05-07 18:14:38 +1000

    trying to fix delete functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -63,3 +63,4 @@ class MoviesController < ApplicationController
 end
 
 
+
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,11 +1,11 @@
 class Movie < ApplicationRecord
   validates :name, presence: true
   validates :release_year, numericality: { greater_than: 1888 }
-  has_many :genre_movies
-  has_many :genres, through: :genre_movies
+  has_many :genre_movies, dependent: :destroy
+  has_many :genres, through: :genre_movies, dependent: :destroy
   belongs_to :director, dependent: :destroy
-  has_many :cast_movies
-  has_many :casts, through: :cast_movies
+  has_many :cast_movies, dependent: :destroy
+  has_many :casts, through: :cast_movies, dependent: :destroy
   belongs_to :user
   belongs_to :country
   has_one_attached :picture
