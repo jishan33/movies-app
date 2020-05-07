cd4a30beaa28c59b9dd699d5ec3be7460ca40167
Lanzhou Jiang <lzjiang99@gmail.com>
2020-05-07 16:38:49 +1000

    Add test for testing associations between Movie and User

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  let(:director) { Director.new }
+  director = Director.create(name: 'An Li') 
+  user = User.create(email: 'User@gmail.com', password: 'password') 
+  country = Country.create(name: 'Australia') 
   subject { described_class.new(
     name: 'Lord of Rings',
-    release_year: '2001',
-    director: director
+    release_year: 2001,
+    director_id: 1,
+    country_id: 1,
+    user_id: 1 
   )}
 
   it 'is vaild with valid attributes' do
@@ -20,6 +24,11 @@ RSpec.describe Movie, type: :model do
   it 'is not valid if release year is less than 1888' do
     subject.release_year = 1700
     expect(subject).to_not be_valid
+  end
+
+  it 'belongs to a User' do
+    relation = Movie.reflect_on_association(:user)
+    expect(relation.macro).to eql(:belongs_to)
   end
   
 end
