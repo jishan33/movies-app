f2e67db27317a24a46871f8d3ccb7dfcadce396c
Ji Shan <uug54332017@gmail.com>
2020-05-07 10:43:40 +1000

    seed data created

---
+++ db/data/data.rb
@@ -0,0 +1,54 @@
+def movies [
+ {
+   name: 'Lord of Rings',
+   released_year: 2001,
+   rating: 8.8,
+   country: 'New Zealand',
+   length: '208 mins',
+   user_id: 1,
+   director_id: 1
+ },
+ 
+ {
+   name: 'The Hobbit',
+   released_year: 2012,
+   rating: 7.8,
+   country: 'New Zealand',
+   length: '149 mins',
+   user_id: 1,
+   director_id: 1
+ },
+ 
+  {
+   name: 'The Godfather',
+   released_year: 1972,
+   rating: 9.0,
+   country: 'America',
+   length: '175 mins',
+   user_id: 1,
+   director_id: 1
+ },
+ 
+ {
+   name: 'Inception',
+   released_year: 2010,
+   rating: 8.8,
+   country: 'America',
+   length: '148 mins',
+   user_id: 1,
+   director_id: 1
+ },
+  {
+   name: 'The Matrix',
+   released_year: 1999,
+   rating: 8.7,
+   country: 'Australia',
+   length: '136 mins',
+   user_id: 1,
+   director_id: 1
+ },
+ 
+ 
+ 
+]
+
